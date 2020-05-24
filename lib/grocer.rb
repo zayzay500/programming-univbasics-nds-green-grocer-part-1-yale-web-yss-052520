3d5e812e8f1cbba0bcfce20b4600ff0cce3ea06b
Lindsay Pierce <lindsay.pierce@yale.edu>
2020-05-24 22:02:07 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,8 +1,12 @@
+require 'pry'
 def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
-
+  collection.find do |item|
+    item[:item] == name
+    # binding.pry
+  end
 end
 
 def consolidate_cart(cart)
